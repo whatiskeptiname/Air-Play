a5c9bd06a7ce2e7d15753c7b5d55e3c2562d17ce
susang <susangmree@gmail.com>
2021-07-20 23:10:51 +0545

    refactored

--- Air-Play.ino
+++ Air-Play.ino
@@ -1,28 +1,27 @@
-// Plays a tone as an object is moved above the ultrasonic sensor array
-
-#define trig1 22
+#define trig1 22 // pin declaration for ultrasonic sensor 1 
 #define echo1 23
 
-#define trig2 24
+#define trig2 24 // pin declaration for ultrasonic sensor 2
 #define echo2 25
 
-#define trig3 26
+#define trig3 26 // pin declaration for ultrasonic sensor 3
 #define echo3 27
 
-#define trig4 28
+#define trig4 28 // pin declaration for ultrasonic sensor 4
 #define echo4 29
 
-#define trig5 30
+#define trig5 30 // pin declaration for ultrasonic sensor 5
 #define echo5 31
 
-#define buzz 11
+#define buzz 11 // pin declaration for buzzer
 
-int s, s1, s2, s3, s4, s5, adf = 0;
+int s1, s2, s3, s4, s5; // proximity from respective ultrasonic sensor (s1 - s5) 
 
-void setup()
+void setup() 
 {
-  Serial.begin(9600);
+  Serial.begin(9600); // set up serial communication at 9600 baud
 
+// I/O pin declaration 
   pinMode(trig1, OUTPUT);
   pinMode(echo1, INPUT);
 
@@ -41,7 +40,7 @@ void setup()
   pinMode(buzz, OUTPUT);
 }
 
-int sensor(int trig, int echo)
+int proximity(int trig, int echo) // function to calculate the proximity of an object from a ultrasonic sensor
 {
   digitalWrite(trig, LOW);
   delay(50);
@@ -52,70 +51,60 @@ int sensor(int trig, int echo)
   return int(duration * 0.034 / 2);
 }
 
-void get_data()
-{
-  s1 = sensor(trig1, echo1);
-  s2 = sensor(trig2, echo2);
-  s3 = sensor(trig3, echo3);
-  s4 = sensor(trig4, echo4);
-  s5 = sensor(trig5, echo5);
-}
+int base_frequency(int s1, int s2, int s3, int s4, int s5, int s) 
+  {// function to  select the base frequency based on the position of sensor with lowest proximity
 
-void play_me_hard(int s, int adf)
+    int adf = 0; // base frequency to play when individual ultrasonic sensor is trigerred without considering the proximity value
+    if (s == s1) adf = 400;
+    else if (s == s2) adf = 600;
+    else if (s == s3) adf = 800;
+    else if (s == s4) adf = 1000;
+    else if (s == s5) adf = 1200;
+    else (s == 0);
+    return adf;
+  }
+
+void play(int s, int adf) // add proximity factor to base frequency and play the tone at that frequency
 {
-  if (s > 1)
+  if (s >= 5 && s < 10)
   {
-    tone(buzz, 500 + adf);
+    tone(buzz, adf); // play tone at adf frequency for 300 milliseconds
+    delay(300);
+    noTone(buzz); // stop playing the tone
+  }
+  else if (s >= 10 && s < 15)
+  {
+    tone(buzz, adf - 50);
     delay(300);
     noTone(buzz);
-    if (s > 15);
-    {
-      tone(buzz, 525 + adf);
-      delay(300);
-      if (s > 30);
-      {
-        tone(buzz, 550 + adf);
-        delay(300);
-        if (s > 45)
-        {
-          tone(buzz, 575 + adf);
-          delay(300);
-          if (s > 60 && s < 80)
-          {
-            tone(buzz, 600 + adf);
-            delay(300);
-          }
-        }
-      }
-    }
   }
+  else if (s >= 15 && s < 20)
+  {
+    tone(buzz, adf - 100);
+    delay(300);
+    noTone(buzz);
+  }
+  else if (s >= 20 && s < 25)
+  {
+    tone(buzz, adf - 150);
+    delay(300);
+    noTone(buzz);
+  }
+  else tone(buzz, 0);
 }
 
 void loop()
 {
-  get_data();
-  s = min(min(min(min(s1, s2), s3), s4), s5);
-  Serial.println(s);
-  if (s == s1)
-  {
-    adf = 0;
-  }
-  else if (s == s2)
-  {
-    adf = 100;
-  }
-  
-  else if (s == s3) 
-  {
-    adf = 200;
-  }
-  else if (s == s4)
-  {
-    adf = 300;
-  }
-  else 
-  {
-    adf = 1000;
-  }
-  play_me_hard(s, adf);
+  // get proximity from respective ultrasonic sensor
+  s1 = proximity(trig1, echo1);
+  s2 = proximity(trig2, echo2);
+  s3 = proximity(trig3, echo3);
+  s4 = proximity(trig4, echo4);
+  s5 = proximity(trig5, echo5);
+
+  int s = min(min(min(min(s1, s2), s3), s4), s5); // get minimum of all the proximity values
+  int adf = base_frequency(s1, s2, s3, s4, s5, s); // calculate the base frequency
+  Serial.println("s = " + String(s) + " and adf = " + String(adf)); // print s and adf to serial monitor
+  play(s, adf); // play the tone
+
 }
